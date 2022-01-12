f2fa4e7539eb655947f9435e67eb10f362fef5d7
Michael Bennett <mtbennett9010@gmail.com>
2022-01-12 14:23:13 +1300

    fixed paths

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@
 
 import React from "react";
 import About from "./components/About";
-import Contact from "./components/About";
-import Navbar from "./components/About";
-import Projects from "./components/About";
-import Skills from "./components/About";
-import Testimonials from "./components/About";
-
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
 
 export default function App() {
   return (
